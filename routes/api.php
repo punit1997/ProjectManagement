83bb3f77fc7f87d105ee84aa3637cc5fee3f7d2b
punit <punit.lakshwani@razorpay.com>
2019-08-07 17:58:14 +0530

    Add basicauth in all routes
    Admin is creating user so it also require authentication

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
-
-    Route::get('/team', 'TeamController@showTeam')->middleware(['basicauth']);
-    Route::get('/projects', 'ProjectController@showProjects')->middleware(['basicauth']);
-    Route::get('/team_members', 'TeamController@teamMembers')->middleware(['basicauth']);
-    Route::get('/project_members/{project_id}', 'ProjectController@projectMembers')->middleware(['basicauth']);
-    Route::post('/project/create', 'ProjectController@create')->middleware(['basicauth']);
-    Route::post('/project/{projectId}/{userId}', 'ProjectController@addMemberToProject')->middleware(['basicauth']);
-    Route::post('/team/create', 'TeamController@create')->middleware(['basicauth']);
+Route::middleware(['basicauth'])->group(function () {
+    Route::get('/team', 'TeamController@showTeam');
+    Route::get('/projects', 'ProjectController@showProjects');
+    Route::get('/team_members', 'TeamController@teamMembers');
+    Route::get('/project_members/{project_id}', 'ProjectController@projectMembers');
+    Route::post('/project/create', 'ProjectController@create');
+    Route::post('/project/{projectId}/{userId}', 'ProjectController@addMemberToProject');
+    Route::post('/team/create', 'TeamController@create');
     Route::post('/user/create', 'UserController@create');
-    Route::patch('/user/{user_id}', 'UserController@update')->middleware(['basicauth']);
-    Route::delete('/project/{projectId}/{userId}', 'ProjectController@removeMemberFromProject')->middleware(['basicauth']);
-    Route::delete('/project/{projectId}/', 'ProjectController@destroy')->middleware(['basicauth']);
-    Route::post('/invitation/{projectId}/{userId}', 'InvitationController@sendInvitation')->middleware(['basicauth']);    
-    Route::patch('/invitation/{invitationId}/accept', 'InvitationController@acceptInvitation')->middleware(['basicauth']); 
-    Route::patch('/invitation/{invitationId}/reject', 'InvitationController@rejectInvitation')->middleware(['basicauth']);
-    Route::get('/invitations/show', 'InvitationController@showMyInvitations')->middleware(['basicauth']);
+    Route::patch('/user/{user_id}', 'UserController@update');
+    Route::delete('/project/{projectId}/{userId}', 'ProjectController@removeMemberFromProject');
+    Route::delete('/project/{projectId}/', 'ProjectController@destroy');
+    Route::post('/invitation/{projectId}/{userId}', 'InvitationController@sendInvitation');
+    Route::patch('/invitation/{invitationId}/accept', 'InvitationController@acceptInvitation');
+    Route::patch('/invitation/{invitationId}/reject', 'InvitationController@rejectInvitation');
+    Route::get('/invitations/show', 'InvitationController@showMyInvitations');
+});
